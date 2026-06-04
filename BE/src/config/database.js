e537aecdc6093460b8e062b8601235f278fe44a6
Gerry Ajie Pratama <gerryajie@gmail.com>
2026-06-05 00:19:38 +0700

    uopdkhf

--- BE/src/config/database.js
+++ BE/src/config/database.js
@@ -1,15 +1,17 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const sequelize =
+  require("../config/database");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false,
-  }
+const { Sequelize, DataTypes } =
+  require("sequelize");
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.PlcLog = require("./PlcLog")(
+  sequelize,
+  DataTypes
 );
 
-module.exports = sequelize;
+module.exports = db;
--- BE/src/models/index.js
+++ BE/src/models/index.js
@@ -1,30 +1,28 @@
 const { Sequelize, DataTypes } =
   require("sequelize");
 
+require("dotenv").config();
 
 const sequelize = new Sequelize(
-  "plc_monitoring",
-  "root",
-  "",
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: "localhost",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
     logging: false,
   }
 );
 
-
 const db = {};
 
 db.Sequelize = Sequelize;
-
 db.sequelize = sequelize;
-
 
 db.PlcLog = require("./PlcLog")(
   sequelize,
   DataTypes
 );
-
 
 module.exports = db;
